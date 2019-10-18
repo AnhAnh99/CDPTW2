e9421a1e34d26c3867bdfd8559e7c1887c76738e
AnhAnh99 <1999buianh@gmail.com>
2019-10-18 21:59:26 +0700

    ok_ok

--- 111/111-content.php
+++ 111/111-content.php
@@ -49,112 +49,115 @@
                      <img alt="" src="images/logo.png">
                  </div>
                  <div class="col-md-6">
-                     <!-- <form role="search" method="get" class="formSearch">
-                     <div class="searchText">
-                     <input type="text" class="s" placeholder="Search...">                     
-                    </div>
-                        
-                         <div class="category">
-                         <button class="mainmenubtn">category <i class="fas fa-sort-down"></i></button>
-                         <div class="dropdown-child">
-                             <a href="#">USD</a>
-                             <a href="#">EUR</a>
-                             <a href="#">GDP</a>
-                         </div>
-                         <button type="submit" class="searchsubmit" data-translate="general.search.submit">Search</button>
+                     <!-- <form class="form-inline my-2 my-lg-0">
+                         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="width: 50%;
+                                height: 38px;
+                                padding: 0 200px 0 15px;
+                                border: 0;
+                                border-radius-left : 20px;
+                                background-color: transparent;
+                                color: #777;
+                                font-size: 13px;">
+                         <a href="#" data-val="all" class=data-translate="general.search.category">Select category</a>
                      </form> -->
-                     <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="    width: 50%;
-                        height: 38px;
-    padding: 0 200px 0 15px;
-    border: 0;
-    border-radius-left : 20px;
-    background-color: transparent;
-    color: #777;
-    font-size: 13px;">
-                        
-                        <a href="#" data-val="all" class= data-translate="general.search.category" >Select category</a>
-                    </form>
+                     <div class="box">
+                         <div class="container-1">
+                             <input type="search" id="search" placeholder="Search..." />
+                             <span class="icon"><i class="fa fa-search"></i></span>
+                            </div>
+                     </div>
                  </div>
-                 <div class="col-md-3"> 
-                 <div class="row">
-                 <div class="col-md-8 call">
-                    <span>CALL US NOW</span>
-                    <br>
-                    <b>+123 5678 890</b>
-                 </div>
-                 <div class="col-md-4 icon">
-                 <a href="#">
-                 <i class="fas fa-shopping-bag" id="icon1"></i>
-                 <i class="fas fa-caret-down" id="icon2"></i>
-                </a>
-                 </div>
-                 </div>
+                 <div class="col-md-3">
+                     <div class="row">
+                         <div class="col-md-8 call">
+                             <span>CALL US NOW</span>
+                             <br>
+                             <b>+123 5678 890</b>
+                         </div>
+                         <div class="col-md-4 icon">
+                             <a href="#">
+                                 <i class="fas fa-shopping-bag" id="icon1"></i>
+                                 <i class="fas fa-caret-down" id="icon2"></i>
+                             </a>
+                         </div>
+                     </div>
                  </div>
              </div>
          </div>
      </div>
      <div class="bot">
-        <div class="container">
-            <div class="navbar">
-                <a href="#">Home</a>
-                <a href="#">News</a>
-                <div class="dropdown">
-                    <button class="dropbtn">COLLECTIONS 
-                    <i class="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                        <div class="row">
-                        <div class="column">
-                            <h3>Category 1</h3>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                            </div>
-                            <div class="column">
-                            <h3>Category 2</h3>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                            </div>
-                            <div class="column">
-                            <h3>Category 3</h3>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="dropdown">
-                    <button class="dropbtn">PRODUCTS 
-                    <i class="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                        <div class="row">
-                            <div class="column">
-                            <p>Category 1</p>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                            <a href="#"><img src="" alt=""></a>
-                            </div>
-                            <div class="column">
-                            <p>Category 2</p>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                            </div>
-                            <div class="column">
-                            <p>Category 3</p>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                            </div>
-                        </div>
-                    </div>
-                </div> 
-            </div>
-        </div>
+         <div class="container">
+             <div class="navbar">
+                 <a href="#">Home</a>
+                 <div class="dropdown">
+                     <button class="dropbtn">COLLECTIONS
+                         <i class="fa fa-caret-down"></i>
+                     </button>
+                     <div class="dropdown-content">
+                         <div class="row">
+                             <div class="column">
+                                 <h3>Category 1</h3>
+                                 <a href="#">Link 1</a>
+                                 <a href="#">Link 2</a>
+                                 <a href="#">Link 3</a>
+                             </div>
+                             <div class="column">
+                                 <h3>Category 2</h3>
+                                 <a href="#">Link 1</a>
+                                 <a href="#">Link 2</a>
+                                 <a href="#">Link 3</a>
+                             </div>
+                             <div class="column">
+                                 <h3>Category 3</h3>
+                                 <a href="#">Link 1</a>
+                                 <a href="#">Link 2</a>
+                                 <a href="#">Link 3</a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="dropdown">
+                     <button class="dropbtn">PRODUCTS
+                         <i class="fa fa-caret-down"></i>
+                     </button>
+                     <div class="dropdown-content">
+                         <ul class="column">
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <a href="#"><img src="" alt=""></a>
+                     </div>
+                 </div>
+                 <div class="dropdown">
+                     <button class="dropbtn">PAGES
+                         <i class="fa fa-caret-down"></i>
+                     </button>
+                     <div class="dropdown-content">
+                         <ul class="column">
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <a href="#"><img src="" alt=""></a>
+                         </ul>
+                     </div>
+                 </div>
+                 <div class="dropdown">
+                     <button class="dropbtn">FEATURES
+                         <i class="fa fa-caret-down"></i>
+                     </button>
+                     <div class="dropdown-content">
+                         <ul class="column">
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <li> <a href="#">Link 1</a></li>
+                             <a href="#"><img src="" alt=""></a>
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+         </div>
      </div>
  </div>
--- 113/113-content.php
+++ 113/113-content.php
@@ -3,14 +3,19 @@
     <div class="swiper-wrapper">
         <div class="swiper-slide one">
         <div class="contentleft">
-            <h4>80% off for select items</h4>
-            <h3>fashion mega sale</h3>
+            <h5>
+                <span style="border-color:#a2ccd7;"></span>
+                <b>80% off for select items</b>
+            </h5>
+            <h2>
+                <span><b>fashion mega sale</b></span>
+            </h2>
             <a href="#" class="btn btn-default">Shop Now</a></div>
         </div>
         <div class="swiper-slide two">
         <div class="contentright">
-            <h4>up to 70% off</h4>
-            <h3>women’s hats</h3>
+            <h4><b>up to 70% off</b></h4>
+            <h3><b>women’s hats</b></h3>
             <a href="" class="btn btn-default">Shop Now</a></div>
         </div>
         
